026fb20d277ebee0333498457f377ac5919fa509
Z <141449338+AzamiJs@users.noreply.github.com>
2025-08-20 21:32:05 -0600

    Add

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { gdrive, dropbox, mediafire, fetchDownloadLinks, getDownloadLink } = require('./lib/downloader')
+const { gay } = require('./lib/maker')
 
 module.exports = {
 gdrive, dropbox, mediafire,
-fetchDownloadLinks, getDownloadLink
+fetchDownloadLinks, getDownloadLink,
+
+gay
 }
