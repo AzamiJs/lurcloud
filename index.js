efc6a9d13b6b1bddd072a3b85ab449dba9b8ce8b
Z <141449338+AzamiJs@users.noreply.github.com>
2025-08-20 23:08:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const { gdrive, dropbox, mediafire, fetchDownloadLinks, getDownloadLink } = require('./lib/downloader')
-const { rainbow, blur, invert, blue, greyscale } = require('./lib/maker')
+const { rainbow, blur, invert, blue, greyscale, circle } = require('./lib/maker')
 
 module.exports = {
 gdrive, dropbox, mediafire,
 fetchDownloadLinks, getDownloadLink,
 
-rainbow, blur, invert, blue, greyscale
+rainbow, blur, invert, blue,
+greyscale, circle
 }
